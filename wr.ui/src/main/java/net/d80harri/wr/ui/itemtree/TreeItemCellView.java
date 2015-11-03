65c44109a036d502ee00d9b4d08f794492abd28f
Harald <harald.bittermann@gmail.com>
2015-11-03 11:38:25 +0100

    bugfix

--- wr.ui/src/main/java/net/d80harri/wr/ui/itemtree/TreeItemCellView.java
+++ wr.ui/src/main/java/net/d80harri/wr/ui/itemtree/TreeItemCellView.java
@@ -123,30 +123,38 @@ public class TreeItemCellView extends ViewBase<TreeItemCellPresenter> implements
 			if (evt.getCode() == KeyCode.SPACE) {
 				this.fireEvent(new TreeItemCellEvent(
 						TreeItemCellEvent.TOGGLE_EXPAND));
+				evt.consume();
 			}
 		} else if (evt.isShiftDown()) {
 			if (evt.getCode() == KeyCode.TAB) {
 				this.fireEvent(new TreeItemCellEvent(TreeItemCellEvent.OUTDENT));
+				evt.consume();
 			} else if (evt.getCode() == KeyCode.ENTER) {
 				setDetailVisible(!isDetailVisible());
 				descriptionArea.requestFocus();
+				evt.consume();
 			}
 		} else if (evt.isAltDown()) {
 			if (evt.getCode() == KeyCode.UP) {
 				this.fireEvent(new TreeItemCellEvent(TreeItemCellEvent.MOVE_UP));
+				evt.consume();
 			} else if (evt.getCode() == KeyCode.DOWN) {
 				this.fireEvent(new TreeItemCellEvent(
 						TreeItemCellEvent.MOVE_DOWN));
+				evt.consume();
 			} else if (evt.getCode() == KeyCode.RIGHT) {
 				this.fireEvent(new TreeItemCellEvent(TreeItemCellEvent.INDENT));
+				evt.consume();
 			} else if (evt.getCode() == KeyCode.LEFT) {
 				this.fireEvent(new TreeItemCellEvent(TreeItemCellEvent.OUTDENT));
+				evt.consume();
 			}
 		} else {
 			if (evt.getCode() == KeyCode.ENTER) {
 				if (txtTitle.getCaretPosition() == txtTitle.getText().length()) {
 					this.fireEvent(new TreeItemCellEvent(
 							TreeItemCellEvent.CREATE_AFTER));
+					evt.consume();
 				} else {
 					int caretPosition = getTxtTitle().getCaretPosition();
 					String newText = getTxtTitle().getText().substring(
@@ -157,45 +165,54 @@ public class TreeItemCellView extends ViewBase<TreeItemCellPresenter> implements
 							TreeItemCellEvent.CREATE_AFTER);
 					cellEvent.setTitle(newText);
 					this.fireEvent(cellEvent);
+					evt.consume();
 				}
 			} else if (evt.getCode() == KeyCode.UP) {
 				this.fireEvent(new TreeItemCellEvent(
 						TreeItemCellEvent.GOTO_PREVIOUS));
+				evt.consume();
 			} else if (evt.getCode() == KeyCode.DOWN) {
 				this.fireEvent(new TreeItemCellEvent(
 						TreeItemCellEvent.GOTO_NEXT));
+				evt.consume();
 			} else if (evt.getCode() == KeyCode.TAB) {
 				this.fireEvent(new TreeItemCellEvent(TreeItemCellEvent.INDENT));
+				evt.consume();
 			} else if (evt.getCode() == KeyCode.BACK_SPACE) {
 				if (txtTitle.getText().isEmpty()) {
 					this.fireEvent(new TreeItemCellEvent(
 							TreeItemCellEvent.DELETE));
+					evt.consume();
 				} else if (txtTitle.getCaretPosition() == 0) {
 					this.fireEvent(new TreeItemCellEvent(
 							TreeItemCellEvent.MERGEWITH_PREVIOUS));
+					evt.consume();
 				}
 			} else if (evt.getCode() == KeyCode.DELETE) {
 				if (txtTitle.getText().isEmpty()) {
 					this.fireEvent(new TreeItemCellEvent(
 							TreeItemCellEvent.DELETE));
+					evt.consume();
 				} else if (txtTitle.getCaretPosition() == txtTitle.getText()
 						.length()) {
 					this.fireEvent(new TreeItemCellEvent(
 							TreeItemCellEvent.MERGEWITH_NEXT));
+					evt.consume();
 				}
 			} else if (evt.getCode() == KeyCode.LEFT) {
 				if (txtTitle.getCaretPosition() == 0) {
 					this.fireEvent(new TreeItemCellEvent(
 							TreeItemCellEvent.GOTO_PREVIOUS));
+					evt.consume();
 				}
 			} else if (evt.getCode() == KeyCode.RIGHT) {
 				if (txtTitle.getCaretPosition() == txtTitle.getText().length()) {
 					this.fireEvent(new TreeItemCellEvent(
 							TreeItemCellEvent.GOTO_NEXT));
+					evt.consume();
 				}
 			}
 		}
-		evt.consume();
 	}
 
 	public Pane getDetailPane() {
